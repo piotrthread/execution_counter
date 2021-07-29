5a6977f81518775f1faf6f962b668fc77952e245
Piotr Niciejewski <piotrthread@gmail.com>
2021-07-29 23:23:38 +0200

    added tng

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import styled from "styled-components";
 import { createGlobalStyle } from "styled-components";
-import { useState } from "react";
-import useSound from "use-sound";
-import catch_sound from "./sounds/catch.wav";
-import miss_sound from "./sounds/miss.wav";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ExecutionCounter from "./ExecutionCounter";
+import ThrowNGoLevel1 from "./ThrowNGoLevel1";
 
 const GlobalStyle = createGlobalStyle`
 *{
@@ -14,133 +12,22 @@ font-family: 'Press Start 2P';
 }
 `;
 
-const Catches = styled.div`
-  background-color: #00e017;
-  height: 100%;
-  flex-basis: 50%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-`;
-
-const Misses = styled.div`
-  background-color: #ff1962;
-  flex-basis: 50%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-`;
-const Text = styled.p`
-  font-size: 20vw;
-  color: white;
-`;
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  width: 100vw;
-  height: 100vh;
-  position: relative;
-  justify-content: center;
-`;
-
-const ResultPercent = styled.p`
-  background-color: white;
-  padding: 15px 20px;
-  text-align: center;
-  color: black;
-  position: absolute;
-  bottom: 0;
-  font-size: 4vw;
-`;
-const ResultPoints = styled.p`
-  background-color: white;
-  padding: 15px 20px;
-  color: black;
-  position: absolute;
-  top: 0;
-  font-size: 4vw;
-`;
-
-const ResetButton = styled.p`
-  padding: 15px 20px;
-  color: white;
-  position: absolute;
-  top: 4px;
-  right: 0;
-  font-size: 4vw;
-  cursor: pointer;
-`;
-
-const ChangeCatch = styled.p`
-  padding: 15px 20px;
-  color: white;
-  position: absolute;
-  bottom: 4px;
-  right: 0;
-  font-size: 4vw;
-  cursor: pointer;
-`;
-const ChangeMiss = styled.p`
-  padding: 15px 20px;
-  color: white;
-  position: absolute;
-  bottom: 4px;
-  left: 0;
-  font-size: 4vw;
-  cursor: pointer;
-`;
-
 const App = () => {
-  const [catches, setCatches] = useState(0);
-  const [misses, setMisses] = useState(0);
-  const [playCatchSound] = useSound(catch_sound);
-  const [playMissSound] = useSound(miss_sound);
   return (
-    <Wrapper>
-      <ChangeMiss onClick={() => setMisses(misses - 1)}>BACK</ChangeMiss>
-      <ChangeCatch onClick={() => setCatches(catches - 1)}>BACK</ChangeCatch>
-      <ResetButton
-        onClick={() => {
-          setCatches(0);
-          setMisses(0);
-        }}
-      >
-        RESET
-      </ResetButton>
-      <Misses
-        onClick={() => {
-          setMisses(misses + 1);
-          playMissSound();
-        }}
-      >
-        <Text>{misses}</Text>
-      </Misses>
-      <Catches
-        onClick={() => {
-          setCatches(catches + 1);
-          playCatchSound();
-        }}
-      >
-        <Text>{catches}</Text>
-      </Catches>
-      <ResultPoints>
-        {catches + misses <= 18
-          ? ((catches / 18) * 10).toFixed(2)
-          : ((catches / (catches + misses)) * 10).toFixed(2)}{" "}
-        pts
-      </ResultPoints>
-      <ResultPercent>
-        {catches + misses <= 18
-          ? ((catches / 18) * 100).toFixed(2)
-          : ((catches / (catches + misses)) * 100).toFixed(2)}
-        %
-      </ResultPercent>
+    <Router>
+      <Switch>
+        <Route path="/execution">
+          <ExecutionCounter />
+        </Route>
+        <Route path="/throwngo1">
+          <ThrowNGoLevel1 />
+        </Route>
+        <Route path="/">
+          <h1>Menu</h1>
+        </Route>
+      </Switch>
       <GlobalStyle />
-    </Wrapper>
+    </Router>
   );
 };
 
---
+++ src/ThrowNGoLevel1.js
@@ -0,0 +1,325 @@
+import { useState } from "react";
+import styled from "styled-components";
+import catch_sound from "./sounds/catch.wav";
+import useSound from "use-sound";
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  padding: 50px;
+  display: flex;
+  background-color: #00f0ff;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+const CenterRow = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 18vw;
+`;
+const SideRow = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 9vw;
+`;
+const MenuRow = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+`;
+
+const CenterField = styled.div`
+  width: 18vw;
+  border: 3px solid white;
+  cursor: pointer;
+  background-color: rgba(255, 255, 255, 0.6);
+`;
+
+const StartLeft = styled.div`
+  position: relative;
+  width: 18vw;
+  border: 3px solid white;
+  cursor: pointer;
+  background-color: rgba(255, 255, 255, 0.6);
+
+  &:after {
+    position: absolute;
+    display: inline-block;
+    content: "START";
+    z-index: 9999;
+    color: white;
+    font-size: 25px;
+    transform: rotate(90deg);
+    left: -35px;
+    top: 120px;
+  }
+`;
+
+const StartRight = styled.div`
+  position: relative;
+  width: 18vw;
+  border: 3px solid white;
+  cursor: pointer;
+  background-color: rgba(255, 255, 255, 0.6);
+
+  &:after {
+    position: absolute;
+    display: inline-block;
+    content: "START";
+    z-index: 9999;
+    color: white;
+    font-size: 25px;
+    transform: rotate(-90deg);
+    right: -35px;
+    top: 120px;
+  }
+`;
+
+const SideField = styled.div`
+  width: 18vw;
+  border: 3px solid white;
+  cursor: pointer;
+  background-color: rgba(255, 255, 255, 0.3);
+`;
+
+const Header = styled.p`
+  margin: 40px;
+  font-size: 40px;
+  color: white;
+`;
+
+const Reset = styled.p`
+  margin: 40px;
+  font-size: 40px;
+  cursor: pointer;
+  color: white;
+`;
+
+const ThrowNGoLevel1 = () => {
+  const [points, setPoints] = useState(0);
+  const [side, setSide] = useState(false);
+  const [playCatchSound] = useSound(catch_sound);
+  return (
+    <Wrapper>
+      <MenuRow>
+        <Header>{points} pts</Header>
+        <Reset onClick={() => setPoints(0)}>RESET</Reset>
+      </MenuRow>
+
+      {side ? (
+        <>
+          <SideRow>
+            <SideField
+              onClick={() => {
+                setPoints(points + 6);
+                playCatchSound();
+              }}
+            />
+            <SideField
+              onClick={() => {
+                setPoints(points + 5);
+                playCatchSound();
+              }}
+            />
+            <SideField
+              onClick={() => {
+                setPoints(points + 4);
+                playCatchSound();
+              }}
+            />
+            <SideField
+              onClick={() => {
+                setPoints(points + 3);
+                playCatchSound();
+              }}
+            />
+            <SideField
+              onClick={() => {
+                setPoints(points + 1);
+                playCatchSound();
+              }}
+            />
+          </SideRow>
+          <CenterRow>
+            <CenterField
+              onClick={() => {
+                setPoints(points + 12);
+                playCatchSound();
+              }}
+            />
+            <CenterField
+              onClick={() => {
+                setPoints(points + 10);
+                playCatchSound();
+              }}
+            />
+            <CenterField
+              onClick={() => {
+                setPoints(points + 8);
+                playCatchSound();
+              }}
+            />
+            <CenterField
+              onClick={() => {
+                setPoints(points + 6);
+                playCatchSound();
+              }}
+            />
+            <StartRight
+              onClick={() => {
+                setPoints(points + 1);
+                playCatchSound();
+              }}
+            />
+          </CenterRow>
+          <SideRow>
+            <SideField
+              onClick={() => {
+                setPoints(points + 6);
+                playCatchSound();
+              }}
+            />
+            <SideField
+              onClick={() => {
+                setPoints(points + 5);
+                playCatchSound();
+              }}
+            />
+            <SideField
+              onClick={() => {
+                setPoints(points + 4);
+                playCatchSound();
+              }}
+            />
+            <SideField
+              onClick={() => {
+                setPoints(points + 3);
+                playCatchSound();
+              }}
+            />
+            <SideField
+              onClick={() => {
+                setPoints(points + 1);
+                playCatchSound();
+              }}
+            />
+          </SideRow>
+        </>
+      ) : (
+        <>
+          <SideRow>
+            <SideField
+              onClick={() => {
+                setPoints(points + 1);
+                playCatchSound();
+              }}
+            />
+            <SideField
+              onClick={() => {
+                setPoints(points + 3);
+                playCatchSound();
+              }}
+            />
+            <SideField
+              onClick={() => {
+                setPoints(points + 4);
+                playCatchSound();
+              }}
+            />
+            <SideField
+              onClick={() => {
+                setPoints(points + 5);
+                playCatchSound();
+              }}
+            />
+            <SideField
+              onClick={() => {
+                setPoints(points + 6);
+                playCatchSound();
+              }}
+            />
+          </SideRow>
+          <CenterRow>
+            <StartLeft
+              onClick={() => {
+                setPoints(points + 1);
+                playCatchSound();
+              }}
+            />
+            <CenterField
+              onClick={() => {
+                setPoints(points + 6);
+                playCatchSound();
+              }}
+            />
+            <CenterField
+              onClick={() => {
+                setPoints(points + 8);
+                playCatchSound();
+              }}
+            />
+            <CenterField
+              onClick={() => {
+                setPoints(points + 10);
+                playCatchSound();
+              }}
+            />
+            <CenterField
+              onClick={() => {
+                setPoints(points + 12);
+                playCatchSound();
+              }}
+            />
+          </CenterRow>
+          <SideRow>
+            <SideField
+              onClick={() => {
+                setPoints(points + 1);
+                playCatchSound();
+              }}
+            />
+            <SideField
+              onClick={() => {
+                setPoints(points + 3);
+                playCatchSound();
+              }}
+            />
+            <SideField
+              onClick={() => {
+                setPoints(points + 4);
+                playCatchSound();
+              }}
+            />
+            <SideField
+              onClick={() => {
+                setPoints(points + 5);
+                playCatchSound();
+              }}
+            />
+            <SideField
+              onClick={() => {
+                setPoints(points + 6);
+                playCatchSound();
+              }}
+            />
+          </SideRow>
+        </>
+      )}
+
+      <MenuRow>
+        <Reset onClick={() => setSide(!side)}>SWITCH SIDE</Reset>
+        <Reset
+          onClick={() => {
+            setPoints(points + 5);
+            playCatchSound();
+          }}
+        >
+          SWEET SPOT
+        </Reset>
+      </MenuRow>
+    </Wrapper>
+  );
+};
+
+export default ThrowNGoLevel1;
